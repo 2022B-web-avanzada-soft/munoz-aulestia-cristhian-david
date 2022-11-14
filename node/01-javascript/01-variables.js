4de0db8ce7ff7b1fe8c85bc03f967b5c20b8b7c8
cristhianmunoz <cristhian.munoz@epn.edu.ec>
2022-11-14 18:24:12 -0500

    First commit

---
+++ node/01-javascript/01-variables.js
@@ -0,0 +1,26 @@
+
+//variables mutables (re asignada) e inmutables
+
+var numeroUno = 1;
+let numeroDos = 2;
+numeroUno = 12;
+numeroDos = 8;
+nuemroUno = false;
+
+//Inmutables
+const configuracionArchivos = 'PDF';
+// ERROR -> configuracionArchivos = 'xml';
+//vamos a preferir CONST > LET > NUNCA VAR
+
+//Tipos de variables (primitivas)
+const numero = 1; //number
+const sueldo = 1.2; //number
+const texto =  'Crissth'; // String
+const apellido =  "Munoz"; // String
+const booleano = true; // boolean
+const hijos = null ; //object
+const zapatos= undefined; //undefined
+
+console.log(typeof numero)
+
+//node 01-variables.js
