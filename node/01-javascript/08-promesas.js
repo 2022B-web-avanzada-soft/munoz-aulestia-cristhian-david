fc6deda96167a38ac40a0865a8b89b5047f06aba
cristhianmunoz <cristhian.munoz@epn.edu.ec>
2022-12-09 21:50:20 -0500

    Promesas- Ejercicio Completado

--- node/01-javascript/08-promesas.js
+++ node/01-javascript/08-promesas.js
@@ -6,49 +6,48 @@ const fs = require('fs');
     Utilizar eL modulo 'fs' para Leer eL archivo en ese "path" y anadir el
     "contenidoArchivo" a ese archivo.
 */
-function promesaEsPar (numero){// f->promesa
-    const miPrimerPromesa = new Promise(
-        (resolve,reject) =>{
-            if( numero % 2 === 0) { resolve(numero); /*return (then)*/ }
-            else{ reject (':( no es par'); /*throw (catch)*/}
+
+function leerArchivos(path){
+    return new Promise(
+        (resolve, reject) =>{
+            fs.readFile(
+                path,
+                (errorLecturaArchivo, contenidoArchivo) => {
+                    if (errorLecturaArchivo){
+                        reject('No se leyó el archivo ;(')
+                    } else{
+                        resolve(contenidoArchivo);
+                    }
+                }
+            );
         }
     );
-    return miPrimerPromesa;
-}
-function leerArchivos(path){
-
 }
 function escribirArchivos(path, contenidoArchivo){
-
+    return new Promise(
+        (resolve,reject) =>{
+            fs.writeFile(
+                path,
+                contenidoArchivo,
+                (errorEscritura) => {
+                    if (errorEscritura){
+                        reject('No se escribio en el archivo ;(')
+                    }else{
+                        console.log('Si se escribio');
+                    }
+                }
+            );
+        }
+    );
 }
-
-fs.readFile(
-    './01-variables.js',
-    (errorLecturaArchivo2, contenidoArchivo2) => {
-        if (errorLecturaArchivo2){
-            console.log('Error leyendo Archivo1', errorLecturaArchivo2);
-        } else{
-            console.log('Contenido', contenidoArchivo2);
-            file2 = contenidoArchivo2;
-        }
-    }
-);
-
-fs.writeFile(
-    './06-nuevo-archivo.txt',
-    file1= file1 + file2,
-    (errorEscritura) => {
-        if (errorEscritura){
-            console.log('Error escribiendo en Archivo:', errorLecturaArchivo2);
-        }else{
-            console.log('Exito en la escritura en el nuevo archivo');
-        }
-    }
-);
 
 function ejercicio08(path, contenidoArchivo){
     return leerArchivos(path)
-        .then(())
+        .then(
+            (contenido)=>{
+                return escribirArchivos(path,contenido + contenidoArchivo);
+            }
+        )
 
 }
 
